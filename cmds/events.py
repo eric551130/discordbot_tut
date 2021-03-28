f69885266ec8308210b833dcbb01c7e22d53fb67
BBLG <eric558994@gmail.com>
2021-03-28 17:33:00 +0800

    錯誤處理

--- cmds/events.py
+++ cmds/events.py
@@ -20,8 +20,16 @@ class Event(Cog_Extension):
         await channel.send(F'{member} join!')
         print(F'{member} leave!')
 
-    '''@commands.Cog.listener()
-    async def on_command_error(self,ctx,error):'''
+    @commands.Cog.listener()
+    async def on_command_error(self,ctx,error):
+        if isinstance(error,commands.MissingRequiredArgument):
+            await ctx.send(F'{error}遺失參數')
+        elif isinstance(error,commands.CommandNotFound):
+            await ctx.send(F'{error}查無指令')
+        elif isinstance(error,commands.CommandInvokeError):
+            await ctx.send(F'{error}參數錯誤')
+        else:
+            await ctx.send(F'{error}發生錯誤')
 
 
 def setup(bot):
